a3b088e676ac27d27f9a3f7531c8f231bac64be2
Matt <matt@multicode.co.uk>
2023-01-24 11:38:03 +0000

    starting security

---
+++ src/main/java/com/allstate/payments/service/BootstrapService.java
@@ -0,0 +1,40 @@
+package com.allstate.payments.service;
+
+import com.allstate.payments.data.UserRepository;
+import com.allstate.payments.domain.User;
+import com.allstate.payments.domain.UserRole;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+
+@Service
+public class BootstrapService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserService userService;
+
+    @PostConstruct
+    public void createIntialUsers() {
+        if (userRepository.count() == 0) {
+            User user1 = new User("matt", "Matt", "pass1", UserRole.USER);
+            User user2 = new User("sally", "Sally", "pass1", UserRole.MANAGER);
+            userService.save(user1);
+            userService.save(user2);
+        }
+    }
+}
+
+
+/*
+IOC: Spring will instantiate the classes
+BootstrapService bootstrapService = new BootstrapService();
+@Service, @Repository, @RestController
+
+Dependency Injection: Spring will call a setter method for us
+this.userRepository = userRepository
+@Autowired
+ */
---
+++ src/main/java/com/allstate/payments/service/UserService.java
@@ -0,0 +1,8 @@
+package com.allstate.payments.service;
+
+import com.allstate.payments.domain.User;
+
+public interface UserService {
+
+    public void save(User user);
+}
